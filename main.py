e3201ce50d84ddf2e36e146e1e7068723d53ccf6
Srijan Shrestha <sthas425@gmail.com>
2025-05-23 07:21:58 +0545

    adding ball.py making Ball class and unfixed bounced ball

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,7 +11,9 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
- 
+ball = Ball()
+
+
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -18,6 +22,18 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    #Detect collision with wall
+    if ball.ycor() > 270 or ball.ycor() < -270:
+        ball.bounce()
+
+
+
+
+
+
 
 screen.exitonclick()
