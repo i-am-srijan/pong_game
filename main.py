59b9b83e16ffa5a6fd76e4979f7b94be46f37a03
Srijan Shrestha <sthas425@gmail.com>
2025-05-24 08:24:00 +0545

    completing the pong game

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
+
 
 screen = Screen()
 screen.bgcolor("black")
@@ -12,7 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -30,11 +32,16 @@ while game_is_on:
     if ball.ycor() > 270 or ball.ycor() < -270:
         ball.bounce_y()
 
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
 
-
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
